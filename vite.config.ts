7a869802f4f29edc2036c4852f6962fbbd3a39eb
daniel <david.torres@ticketmaster.com>
2026-01-21 13:33:24 -0600

    fix: icons

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon-96x96.png', 'web-app-manifest-192x192.png', 'web-app-manifest-512x512.png'],
+      includeAssets: ['favicon-96x96.png', 'favicon-192x192.png', 'favicon-512x512.png'],
       manifest: {
         name: 'Activity Tracker',
         short_name: 'Activities',
@@ -24,18 +24,18 @@ export default defineConfig({
         start_url: BASE_URL,
         icons: [
           {
-            src: 'web-app-manifest-96x96.png',
+            src: 'favicon-96x96.png',
             sizes: '96x96',
             type: 'image/png',
             purpose: 'any'
           },
           {
-            src: 'web-app-manifest-192x192.png',
+            src: 'favicon-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'web-app-manifest-512x512.png',
+            src: 'favicon-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'maskable'
